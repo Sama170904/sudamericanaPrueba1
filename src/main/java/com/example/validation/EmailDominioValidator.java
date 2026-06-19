500dceb295454f4324fe47c8cc277fecac273be5
PRE SAMANIEGO CABRERA ROLAND <rsamaniego@sasf.local>
2026-06-19 16:31:43 -0500

    actu

--- src/main/java/com/example/validation/EmailDominioValidator.java
+++ src/main/java/com/example/validation/EmailDominioValidator.java
@@ -23,3 +23,10 @@ public class EmailDominioValidator implements ConstraintValidator<ValidEmailDomi
 //agregar entidad cursos con endpoint que agregue un estudoiante a un curso y un endpoint que liste los cursos
 // con la lista de estudianteque tenga ese curso. 
 //en el endpoint de listar estudaintes agregar un filtro que sea opcional, y que sea el nombre del curso
+
+
+//primera mitad trabajare con spring
+//segunda mitad sql server
+//React y Javascript
+
+//tener un script con insercciones para la base de datos listo
